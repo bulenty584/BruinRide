9fe9646c977048220a3fce35c69c7c65fa26ef36
ejaramillo2003 <ejaramillo2003@icloud.com>
2023-11-12 22:47:21 -0800

    Changed the App.js file to App.jsx and rewrote it to render the new components.
    Created new folder 'Components' where the main components of the webpage will be stored in their individual folders. A topbar and a carousel were developed. Bootsrap is being used to implement these components so it should be installed by the rest of my teammates with npm install.

---
+++ bruin_ride/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './Components/Topbar/Topbar';
+import ControlledCarousel from './Components/Carousel/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div><TopBar /></div>
+      <br />
+      <div><ControlledCarousel /></div>
+    </div>
+  );
+}
+
+export default App;
