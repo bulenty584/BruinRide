3ec01e8bf5b0d56f5f1cc5f8b162f28948a5b1fa
theopatsis <theodore.patsis@gmail.com>
2023-12-01 15:09:43 -0800

    filter trips into current and past, minor styling changes

--- bruin_ride/src/profile/Profile.jsx
+++ bruin_ride/src/profile/Profile.jsx
@@ -1,12 +1,11 @@
-//import './Profile.css';
 import '../MainPage.css';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import TripList from './components/TripList';
 import TopBar from '../main_page/components/Topbar/Topbar';
 import { db } from '../login/SignInOut';
 import { getFirestore, collection, query, orderBy, onSnapshot, doc, setDoc, where, getDocs } from 'firebase/firestore';
 import './profile.css';
+
 const Profile = () => {
   const allTrips = async () => {
     const tripsRef = collection(db, 'trips');
@@ -20,53 +19,76 @@ const Profile = () => {
   };
 
   const linkStyle = {
-    color: 'inherit', // Inherit the color from the parent
-    textDecoration: 'none', // Remove underline
+    color: 'inherit',
+    textDecoration: 'none',
   };
 
-  const [Trips, setTrips] = useState(null)
+  const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = dateTime.toLocaleDateString(undefined, optionsDate);
+    const optionsTime = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+    const time = dateTime.toLocaleTimeString(undefined, optionsTime);
+    return { date, time };
+  };
+
+  const [currentTrips, setCurrentTrips] = useState([]);
+  const [pastTrips, setPastTrips] = useState([]);
 
   useEffect(() => {
-    allTrips().then((result) => {
-      const currentTime = new Date();
-      const pastTrips = result;
-      setTrips(pastTrips); // Update state
-    }).catch((error) => {
-      console.error(error);
-    });
+    allTrips()
+      .then((result) => {
+        const currentTime = new Date();
+        const pastTrips = result.filter((trip) => new Date(trip.dateTime) < currentTime);
+        const upcomingTrips = result.filter((trip) => new Date(trip.dateTime) >= currentTime);
+        setPastTrips(pastTrips);
+        setCurrentTrips(upcomingTrips);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []); // Empty dependency array means this runs once on mount
+
+  const renderTrips = (trips) => {
+    return trips.map((trip) => (
+      <div key={trip.id}>
+        <Link to={`/trip_page/${trip.id}`} style={linkStyle}>
+          <div className="trip-box">
+            <h3>{`${trip.pickupLocation} to LAX`}</h3>
+            {formatDateTime(trip.dateTime) && (
+              <>
+                <p>Date: {formatDateTime(trip.dateTime).date}</p>
+                <p>Time: {formatDateTime(trip.dateTime).time}</p>
+              </>
+            )}
+          </div>
+        </Link>
+      </div>
+    ));
+  };
 
   return (
     <body>
-    <div className="background-circles"></div>
-    <div className="app">
-      <header>
-        <TopBar />
-      </header>
-      <main className='backTrips'>
-        <div className='tripsPage'>
-          <h2>Current Trips</h2>
-          {Trips ? (
-            Trips.map((trip) => (
-              <div key={trip.id}>
-                <Link to={`/trip_page/${trip.id}`} style={linkStyle}>
-                  <div className="trip-box">
-                    <p> Name: {trip.name} </p>
-                    <p>Pickup Point: {trip.pickupLocation}</p>
-                    <p>Time: {trip.time}</p>
-                  </div>
-                </Link>
-              </div>
-            ))
-          ) : (
-            <p>Loading trips...</p> // Show a loading message or spinner
-          )}
-        </div>
-      </main>
-    </div>
+      <div className="background-circles"></div>
+      <div className="app">
+        <header>
+          <TopBar />
+        </header>
+        <main className='backTrips' style={{ paddingTop: '75px' }}>
+          <div className='tripsPage'>
+            <h2>Current Trips</h2>
+            {renderTrips(currentTrips)}
+            {currentTrips.length === 0 && <p>No current trips.</p>}
+          </div>
+          <div className='tripsPage'>
+            <h2>Past Trips</h2>
+            {renderTrips(pastTrips)}
+            {pastTrips.length === 0 && <p>No past trips.</p>}
+          </div>
+        </main>
+      </div>
     </body>
   );
 };
 
 export default Profile;
-
