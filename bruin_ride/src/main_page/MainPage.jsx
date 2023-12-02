d5f9390497a706bce550b01fee68213a35d23912
xdeliax <deliaivascu79@gmail.com>
2023-12-02 00:55:35 -0800

    home page

--- bruin_ride/src/main_page/MainPage.jsx
+++ bruin_ride/src/main_page/MainPage.jsx
@@ -1,20 +1,34 @@
 import '../MainPage.css';
 import React from 'react';
-import divbox from '../images/Rectangle.png'
+import image from '../images/homepage.svg'
 import TopBar from './components/Topbar/Topbar';
-import ControlledCarousel from './components/Carousel/Carousel';
+
+/* if signed in, change text to view ur upcoming trips, and my rides */
 
 function MainPage() {
   return(
-    <body>
-    <div className="background-circles"></div>
     <div className="App">
-      <TopBar />
-      <main className='main'>  
-        
+        <div className="background-circles"></div>
+        <TopBar />
+        <main className='main'>  
+        <div class="hero-section">
+          <div class="col">
+            <div>
+            <p className='header'>Ridesharing <br></br>made easy.</p>
+            <p className='subheader'>from bruins, for bruins</p>
+            </div>
+            <button>FIND A RIDE</button>
+          </div>
+          <div class="col">
+          <img src={image}/>
+          </div>
+        </div>
+        <div className="get-started">
+          <p className="header2">Plan Your Next <br></br>Airport Ride Now</p>
+          <button class="startbutton">GET STARTED</button>
+        </div>
       </main>
     </div>
-    </body>
   );
 }
 
